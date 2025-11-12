42e0dd3a957e6ff0e3bb9167a7615a0e4e617733
jardison17 <jallima1997@gmail.com>
2025-11-11 22:13:31 -0300

    inicio1

---
+++ android/app/src/main/kotlin/com/example/forklift/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.forklift
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Forklift());
+}
+
+class Forklift extends StatelessWidget {
+  const Forklift({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.menu, color: Colors.white, size: 30),
+            Padding(padding: EdgeInsets.all(10)),
+            Text(
+              'ForkLift',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.amber,
+              width: double.infinity,
+              height: 48.0,
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [Text('Selecione a empilhadeira')],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
