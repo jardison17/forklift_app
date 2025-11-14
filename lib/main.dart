5506b0dc745ece6965c329761b5e2b8a4fefc8a5
jardison17 <jallima1997@gmail.com>
2025-11-13 22:42:50 -0300

    inicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:forklift/screens/homepage_screen.dart';
 
 void main() {
   runApp(Forklift());
@@ -32,57 +33,21 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Row(
           children: [
-            Icon(Icons.menu, color: Colors.white, size: 30),
+            Icon(Icons.menu_open, color: Colors.white, size: 30),
             Padding(padding: EdgeInsets.all(10)),
             Text(
               'ForkLift',
               style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
+                fontSize: 25,
+                color: Color(0xFFFC6E20),
                 fontWeight: FontWeight.w900,
               ),
             ),
           ],
         ),
-        backgroundColor: Colors.deepOrangeAccent,
+        backgroundColor: Color(0xFF0F0F0F),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.deepOrangeAccent,
-              width: double.infinity, // largura infinita
-
-              padding: const EdgeInsets.all(12), // Margem interna
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-
-                children: [
-                  Text(
-                    'Selecione a empilhadeira',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Buscar Empilhadeiras',
-                      labelStyle: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: HomepageScreen(),
     );
   }
 }
