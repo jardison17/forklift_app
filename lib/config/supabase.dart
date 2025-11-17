ffe3e8ee82d0b26ce8c095538775c8c24a2dd84b
jardison17 <jallima1997@gmail.com>
2025-11-16 21:32:58 -0300

    supabase

--- lib/config/supabase.dart
+++ lib/config/supabase.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static init() async {
-    Supabase.initialize(
+  static Future<void> init() async {
+    await Supabase.initialize(
       url: const String.fromEnvironment('SUPABASE_URL'),
       anonKey: const String.fromEnvironment('SUPABASE_KEY'),
     );
